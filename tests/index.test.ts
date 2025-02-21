6dd04293420565578731d0d14c2d505f42f10abc
= <=>
2025-02-20 22:11:30 -0600

    remove tests

--- tests/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "vitest";
-import * as index from "../src/index";
-import { getKindeSession } from "../src/get-kinde-session";
-import { handleAuth } from "../src/handle-auth";
-
-test("exports the correct functions", () => {
-  expect(index.getKindeSession).toBe(getKindeSession);
-  expect(index.handleAuth).toBe(handleAuth);
-});
